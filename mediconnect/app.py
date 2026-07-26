2bd0f25e638ebdda4041ac03dda278641f2a7ca4
Vekshitha Kutagulla <vekshitha@srmap.edu.in>
2026-07-26 22:22:48 +0530

    Create database tables on startup

--- mediconnect/app.py
+++ mediconnect/app.py
@@ -797,4 +797,6 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_demo_doctors()
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
